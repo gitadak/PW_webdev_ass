57641d26da19d4c1bece3cac90316fb368faeab4
gitadak <138775532+gitadak@users.noreply.github.com>
2023-09-04 10:39:17 +0530

    Add files via upload

---
+++ sing_circ.c
@@ -0,0 +1,338 @@
+//Circular single linked list
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+	int data;
+	struct node *next;
+};
+typedef struct node node;
+
+node* create(int item)
+{
+	node *newnode;
+	newnode=(node*)malloc(sizeof(node));
+	newnode->data=item;
+	newnode->next=NULL;
+	return newnode;
+}
+
+int check(int item,node *head)
+{
+	node *c;
+	c=head;
+	do
+	{
+		if(c->data==item)
+			return 1;
+		c=c->next;
+	}while(c!=head);
+	return 0;
+}
+
+node* insert_end(node* newnode,node* head)
+{
+	node *c;
+	if(head==NULL)
+	{
+		head=newnode;
+		newnode->next=head;
+		return head;
+	}
+	c=head;
+	while(c->next!=head)
+		c=c->next;
+	newnode->next=head;
+	c->next=newnode;
+	return head;
+}
+
+node* insert_beg(node *newnode,node* head)
+{
+	node *c;
+	if(head==NULL)
+	{
+		head=newnode;
+		newnode->next=head;
+		return head;
+	}
+	c=head;
+	while(c->next!=head)
+		c=c->next;
+	c->next=newnode;
+	newnode->next=head;
+	head=newnode;
+	return head;
+}
+
+node* insert_after(node *head,node* newnode,int item)
+{
+	node *c;
+	if(head==NULL)
+		return head;
+	if(check(item,head)==0)
+	{
+		printf("%d is not present\n",item);
+		return head;
+	}
+	c=head;
+	while(c->data!=item)
+		c=c->next;
+	if(c->next==head)
+	{
+		newnode->next=head;
+		c->next=newnode;
+		return head;
+	}
+	newnode->next=c->next;
+	c->next=newnode;
+	return head;
+}
+
+node* insert_before(node* head,node* newnode, int item)
+{
+	node *c;
+	if(head==NULL)
+		return head;
+	if(check(item,head)==0)
+	{
+		printf("%d is not present\n",item);
+		return head;
+	}
+	if(head->data==item)
+	{
+		c=head;
+		while(c->next!=head)
+			c=c->next;
+		c->next=newnode;
+		newnode->next=head;
+		head=newnode;
+		return head;
+	}
+	c=head;
+	while((c->next)->data!=item)
+		c=c->next;
+	newnode->next=c->next;
+	c->next=newnode;
+	return head;
+}
+
+node* del_beg(node *head)
+{
+	node *c,*del;
+	if(head==NULL)
+		return head;
+	if(head->next==head)
+	{
+		del=head;
+		head=NULL;
+		free(del);
+		return head;
+	}
+	c=head;
+	while(c->next!=head)
+		c=c->next;
+	c->next=head->next;
+	del=head;
+	head=head->next;
+	free(del);
+	return head;
+}
+
+node* del_end(node *head)
+{
+	node *c,*del;
+	if(head==NULL)
+		return head;
+	if(head->next==head)
+	{
+		del=head;
+		head=NULL;
+		free(del);
+		return head;
+	}
+	c=head;
+	while((c->next)->next!=head)
+		c=c->next;
+	del=c->next;
+	c->next=head;
+	free(del);
+	return head;
+}
+
+node* del_after(node* head,int item)
+{
+	node *c,*del;
+	if(head==NULL)
+		return head;
+	if(check(item,head)==0)
+	{
+		printf("%d is not present\n",item);
+		return head;
+	}
+	c=head;
+	while(c->data!=item)
+		c=c->next;
+	if(c->next==head)
+	{
+		printf("Last node\n");
+		return head;
+	}
+	del=c->next;
+	c->next=(c->next)->next;
+	free(del);
+	return head;
+}
+
+node* del_before(node *head,int item)
+{
+	node *c,*del;
+	if(head==NULL)
+		return head;
+	if(check(item,head)==0)
+	{
+		printf("%d is not present\n",item);
+		return head;
+	}
+	if(item==head->data)
+	{
+		printf("First node\n");
+		return head;
+	}
+	if((head->next)->data==item)
+	{
+		c=head;
+		while(c->next!=head)
+			c=c->next;
+		c->next=head->next;
+		del=head;
+		head=head->next;
+		free(del);
+		return head;
+	}
+	c=head;
+	while(((c->next)->next)->data!=item)
+		c=c->next;
+	del=c->next;
+	c->next=(c->next)->next;
+	free(del);
+	return head;
+}
+
+void display(node* head)
+{
+	node *c;
+	if(head==NULL)
+	{
+		printf("Empty list\n");
+		return;
+	}
+	printf("List:\n");
+	c=head;
+	do
+	{
+		printf("%3d",c->data);
+		c=c->next;	
+	}
+	while(c!=head);
+}
+
+int main()
+{
+	node* head=NULL,*newnode;
+	int x,data_item,item;
+	char y;
+	do
+	{
+		printf("\n1. Insert\n2. Delete\n3. Display\n4. Exit\nEnter your choice: ");
+		scanf("%d",&x);
+		switch(x)
+		{
+			case 1:
+				do
+				{
+					fflush(stdin);
+					printf("\na. Insert at begin\nb. Insert at end\nc. Insert before an element\nd. Insert after an element\ne. Display\nf. Exit\nEnter your choice: ");
+					scanf("%c",&y);
+					switch(y)
+					{
+						case 'a':
+							printf("Enter the data: ");
+							scanf("%d",&data_item);
+							newnode=create(data_item);
+							head=insert_beg(newnode,head);
+							display(head);
+							break;
+						case 'b':
+							printf("Enter the data: ");
+							scanf("%d",&data_item);
+							newnode=create(data_item);
+							head=insert_end(newnode,head);
+							display(head);
+							break;
+						case 'c':
+							printf("Enter the data: ");
+							scanf("%d",&data_item);
+							newnode=create(data_item);
+							printf("Enter the data before which %d will be inserted: ",newnode->data);
+							scanf("%d",&item);
+							head=insert_before(head,newnode,item);
+							display(head);
+							break;
+						case 'd':
+							printf("Enter the data: ");
+							scanf("%d",&data_item);
+							newnode=create(data_item);
+							printf("Enter the data after which %d will be inserted: ",newnode->data);
+							scanf("%d",&item);
+							head=insert_after(head,newnode,item);
+							display(head);
+							break;
+						case 'e':
+							display(head);
+							break;
+					}
+				} while (y!='f');
+				break;
+			case 2:
+				do
+				{
+					fflush(stdin);
+					printf("\na. Delete at begin\nb. Delete at end\nc. Delete before an element\nd. Delete after an element\ne. Display\nf. Exit\nEnter your choice: ");
+					scanf("%c",&y);
+					switch(y)
+					{
+						case 'a':
+							head=del_beg(head);
+							display(head);
+							break;
+						case 'b':
+							head=del_end(head);
+							display(head);
+							break;
+						case 'c':
+							printf("Enter the data before which deletion will take place: ");
+							scanf("%d",&item);
+							head=del_before(head,item);
+							display(head);
+							break;
+						case 'd':
+							printf("Enter the data after which deletion will take place: ");
+							scanf("%d",&item);
+							head=del_after(head,item);
+							display(head);
+							break;
+						case 'e':
+							display(head);
+							break;
+					} 
+				}while (y!='f');
+				break;
+			case 3:
+				display(head);
+				break;
+		}
+	}while(x!=4);
+	return 0;	
+}
