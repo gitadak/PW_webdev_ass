01d0504fc550dd031d22a651e4223c75b96a6b79
gitadak <138775532+gitadak@users.noreply.github.com>
2023-10-26 19:44:14 +0530

    Add files via upload

---
+++ polymul.c
@@ -0,0 +1,159 @@
+//Polynomial multiplication
+#include<stdio.h>
+#include<stdlib.h>
+
+struct poly
+{
+	int coef;
+	int exp;
+	struct poly *next;
+};
+typedef struct poly poly;
+
+poly* create(int cf,int pw)
+{
+	poly *newpoly = (poly*)malloc(sizeof(poly));
+	newpoly->coef=cf;
+	newpoly->exp=pw;
+	newpoly->next=NULL;
+	return newpoly;
+}
+
+void display(poly *f)
+{
+	poly *c;
+	if(f==NULL)
+	{
+		printf("No polynomial\n");
+		return;
+	}
+	c=f;
+	while(c!=NULL)
+	{
+		printf("%dx^%d + ",c->coef,c->exp);
+		c=c->next;
+	}
+	printf("\b\b ");
+}
+
+poly* insert(poly *f,poly *newpoly)
+{
+	poly *c;
+	if(f==NULL)
+	{
+		f=newpoly;
+		return f;
+	}
+	c=f;
+	while(c->next!=NULL)
+		c=c->next;
+	c->next=newpoly;
+	return f;
+}
+
+poly* poly_add(poly *f1,poly *f2)
+{
+	poly *f3=NULL,*newpoly;
+	if(f1==NULL && f2==NULL)
+	{
+		f3=NULL;
+		return f3;
+	}
+	while(f1!=NULL && f2!=NULL)
+	{
+		if(f1->exp==f2->exp)
+		{
+			newpoly=create(((f1->coef)+(f2->coef)),f1->exp);
+			f3=insert(f3,newpoly);
+			f1=f1->next;
+			f2=f2->next;
+		}
+		else if(f1->exp>f2->exp)
+		{
+			newpoly=create(f1->coef,f1->exp);
+			f3=insert(f3,newpoly);
+			f1=f1->next;
+		}
+		else
+		{
+			newpoly=create(f2->coef,f2->exp);
+			f3=insert(f3,newpoly);
+			f2=f2->next;	
+		}
+	}
+	while(f1!=NULL)
+	{
+		newpoly=create(f1->coef,f1->exp);
+		f3=insert(f3,newpoly);
+		f1=f1->next;
+	}
+	while(f2!=NULL)
+	{
+		newpoly=create(f2->coef,f2->exp);
+		f3=insert(f3,newpoly);
+		f2=f2->next;
+	}
+	return f3;
+}
+
+poly* mul(poly *f,poly *p)
+{
+    poly *i,*s=NULL,*newpoly;
+    for(i=f;i!=NULL;i=i->next)
+    {
+        newpoly=create(p->coef*i->coef,p->exp+i->exp);
+        s=insert(s,newpoly);
+    }
+    return s;    
+}
+
+poly* poly_mul(poly *f1,poly *f2)
+{
+    poly *temp_poly=NULL,*final_poly=NULL,*i;
+    for(i=f1;i!=NULL;i=i->next)
+    {
+        temp_poly=mul(f2,i);
+        final_poly=poly_add(final_poly,temp_poly);
+    }
+    return final_poly;
+}
+
+int main()
+{
+	poly *newpoly,*f1=NULL,*f2=NULL,*f3;
+	int n1,n2,i,cf,pw;
+	printf("\nNo. of terms in first polynomial: ");
+	scanf("%d",&n1);
+	printf("Enter the terms of first polynomial:\n");
+	for(i=0;i<n1;i++)
+	{
+		printf("Term %d:\n",i+1);
+		printf("Enter the co-efficient: ");
+		scanf("%d",&cf);
+		printf("Enter the exponent: ");
+		scanf("%d",&pw);
+		newpoly=create(cf,pw);
+		f1=insert(f1,newpoly);
+	}
+	printf("\nNo. of terms in second polynomial: ");
+	scanf("%d",&n2);
+	printf("Enter the terms of second polynomial:\n");
+	for(i=0;i<n2;i++)
+	{
+		printf("Term %d:\n",i+1);
+		printf("Enter the co-efficient: ");
+		scanf("%d",&cf);
+		printf("Enter the exponent: ");
+		scanf("%d",&pw);
+		newpoly=create(cf,pw);
+		f2=insert(f2,newpoly);
+	}
+	f3=poly_mul(f1,f2);
+	printf("\nPolynomial 1:\n");
+	display(f1);
+	printf("\nPolynomial 2:\n");
+	display(f2);
+	printf("\nResultant polynomial:\n");
+	display(f3);
+	return 0;
+}
